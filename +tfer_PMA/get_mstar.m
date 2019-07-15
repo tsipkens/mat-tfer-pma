682510f9aa45721c21c9136dc5fbd7f130435dd2
Timothy A. Sipkens <tsipkens@gmail.com>
2019-07-15 12:33:42 -0700

    Added get_mstar function

---
+++ +tfer_PMA/get_mstar.m
@@ -0,0 +1,17 @@
+
+% GET_MSTAR Get mass setpoint from provided speeds and voltages
+% Author:   Timothy Sipkens, 2019-15-07
+%=========================================================================%
+
+function [m_star] = get_mstar(prop,V,omega1,omega2)
+
+omega_hat = omega2./omega1;
+
+alpha = omega1.*(prop.r_hat.^2-omega_hat)./(prop.r_hat.^2-1);
+beta = omega1.*prop.r1.^2.*(omega_hat-1)./(prop.r_hat^2-1);
+
+m_star = V./...
+    (log(1/prop.r_hat)./e.*(alpha.*prop.rc+beta./prop.rc).^2);
+
+end
+
